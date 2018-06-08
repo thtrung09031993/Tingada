86cbb4469e3bc41ba67f9e75bb95ad3954e3ac51
minhhy2801 <minhhy2897@gmail.com>
2018-06-08 12:54:37 +0700

    splash, login page

--- app/src/main/java/hci201/tingada/MainActivity.java
+++ app/src/main/java/hci201/tingada/MainActivity.java
@@ -1,13 +1,24 @@
 package hci201.tingada;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    ViewPager viewPager;
+    private TextView txtTitle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.sliderImg);
+        SliderImgLoginActivity sliderImgLoginActivity = new SliderImgLoginActivity(this);
+        viewPager.setAdapter(sliderImgLoginActivity);
     }
+
 }
