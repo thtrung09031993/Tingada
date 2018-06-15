7366da522679339607b6332a51425947fc359228
Hoanh Tung <hoanhtung.xy@gmail.com>
2018-06-15 10:17:33 +0700

    Setting Activity

---
+++ app/src/main/java/hci201/tingada/FindLoverActivity.java
@@ -0,0 +1,22 @@
+package hci201.tingada;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import hci201.tingada.R;
+
+public class FindLoverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_lover);
+    }
+
+    public void clickToSettingProfilePage(View view) {
+    }
+
+    public void clickToContact(View view) {
+    }
+}
---
+++ app/src/main/java/hci201/tingada/SettingActivity.java
@@ -0,0 +1,19 @@
+package hci201.tingada;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import hci201.tingada.R;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+
+    public void clickToBackSettingProfilePage(View view) {
+    }
+}
