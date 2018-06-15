141ca9b1436c1f500f86df8b59529d9fc954beff
Hoanh Tung <hoanhtung.xy@gmail.com>
2018-06-15 12:35:06 +0700

    update all

--- app/src/main/java/hci201/tingada/FindLoverActivity.java
+++ app/src/main/java/hci201/tingada/FindLoverActivity.java
@@ -1,8 +1,12 @@
 package hci201.tingada;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 import hci201.tingada.R;
 
@@ -12,9 +16,6 @@ public class FindLoverActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_lover);
-    }
-
-    public void clickToSettingProfilePage(View view) {
     }
 
     public void clickToContact(View view) {
--- app/src/main/java/hci201/tingada/SettingActivity.java
+++ app/src/main/java/hci201/tingada/SettingActivity.java
@@ -3,17 +3,43 @@ package hci201.tingada;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import hci201.tingada.R;
 
 public class SettingActivity extends AppCompatActivity {
 
+    Switch  menSwt = (Switch) findViewById(R.id.menSwt);
+    Switch  womenSwt = (Switch) findViewById(R.id.womenSwt);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        menSwt.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    womenSwt.setChecked(false);
+                } else {
+                    womenSwt.setChecked(true);
+                }
+            }
+        });
+        womenSwt.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    menSwt.setChecked(false);
+                } else {
+                    menSwt.setChecked(true);
+                }
+            }
+        });
     }
 
     public void clickToBackSettingProfilePage(View view) {
     }
+
+
 }
