5335ae09869c07643daaba639a745a681fb46804
thtrung2006 <thtrung2006@gmail.com>
2018-06-15 16:48:42 +0700

    fix manifest

--- app/src/main/java/hci201/tingada/FeedActivity.java
+++ app/src/main/java/hci201/tingada/FeedActivity.java
@@ -1,7 +1,11 @@
 package hci201.tingada;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class FeedActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class FeedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+        init();
+    }
+
+    private void init(){
+        ImageButton btnProfile = (ImageButton) findViewById(R.id.btnProfile);
+        TextView txtMessage = (TextView) findViewById(R.id.txtMessage);
+        txtMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ContactView.class));
+            }
+        });
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),SettingActivity.class));
+            }
+        });
     }
 }
