807430f6938242e7e96a28893639d5e0dc92a7ef
thtrung2006 <thtrung2006@gmail.com>
2018-06-20 10:55:23 +0700

    premerge

--- app/src/main/java/hci201/tingada/FeedActivity.java
+++ app/src/main/java/hci201/tingada/FeedActivity.java
@@ -1,11 +1,22 @@
 package hci201.tingada;
 
+import android.app.DownloadManager;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class FeedActivity extends AppCompatActivity {
 
